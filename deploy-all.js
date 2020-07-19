e9431bf64c03bdfa20f163fa01b2ca34a578f6c0
Tyler Cobb <tyjcobb94@gmail.com>
2020-07-19 08:37:33 -0500

    Testing deploy all script

---
+++ deploy-all.js
@@ -0,0 +1,16 @@
+const shell = require('shelljs');
+
+const stage = shell.env['AWS_STAGE'];
+if (!stage) {
+    shell.echo("ERROR: Missing required environment variable AWS_STAGE");
+    shell.exit(1);
+}
+
+shell.echo("Deploying apps to stage: " + stage);
+
+shell.cd("apps");
+shell.ls().forEach(function (dir) {
+    shell.cd(dir);
+    shell.exec('sls deploy --stage ' + stage);
+    shell.cd('..');
+});
